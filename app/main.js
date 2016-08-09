1c31c2cc00cb753413b7dd4e5864d75f85e51d50
Marc Johnston <mjohnston@konradgroup.com>
2016-08-09 18:34:24 -0400

    - add rogue attack - zip with interval to apply damage over time

--- app/main.js
+++ app/main.js
@@ -1,23 +1,29 @@
 "use strict";
 var Rx_1 = require('rxjs/Rx');
 var barbarianAttackButton = document.querySelector('.player-list .player:first-child .move.attack');
+var rogueAttackButton = document.querySelector('.player-list .player:last-child .move.attack');
 var dragonAttack = Rx_1.Observable.interval(3000)
     .mapTo({ source: 'Dragon', target: 'Barbarian', damage: 10 });
 var barbarianAttack = Rx_1.Observable.fromEvent(barbarianAttackButton, 'click')
     .throttleTime(1000)
     .mapTo({ source: 'Barbarian', target: 'Dragon', damage: 10 });
-var allAttacks = Rx_1.Observable.merge(barbarianAttack, dragonAttack);
+var poisonStrikeAttack = Rx_1.Observable.of({ source: 'Rogue', target: 'Dragon', damage: 6 }, { source: 'Rogue', target: 'Dragon', damage: 2 }, { source: 'Rogue', target: 'Dragon', damage: 2 }, { source: 'Rogue', target: 'Dragon', damage: 2 }, { source: 'Rogue', target: 'Dragon', damage: 2 })
+    .zip(Rx_1.Observable.interval(500).take(5), function (left, right) { return left; });
+var rogueAttack = Rx_1.Observable.fromEvent(rogueAttackButton, 'click')
+    .throttleTime(1000)
+    .switchMapTo(poisonStrikeAttack);
+var allAttacks = Rx_1.Observable.merge(barbarianAttack, dragonAttack, rogueAttack);
 var dragonDamage = allAttacks.filter(function (x) { return x.target === 'Dragon'; });
 var barbarianDamage = allAttacks.filter(function (x) { return x.target === 'Barbarian'; });
 dragonDamage.subscribe(function (x) {
     var el = document.querySelector('.enemy-list .enemy:first-child .current-hit-points');
     var currentHP = parseInt(el.innerHTML);
-    el.innerHTML = currentHP - x.damage;
+    el.innerHTML = currentHP - x.damage + '';
 });
 barbarianDamage.subscribe(function (x) {
     var el = document.querySelector('.player-list .player:first-child .current-hit-points');
     var currentHP = parseInt(el.innerHTML);
-    el.innerHTML = currentHP - x.damage;
+    el.innerHTML = currentHP - x.damage + '';
 });
 var consoleLogger = allAttacks.subscribe(function (attack) {
     console.log(attack.source + ' hits ' + attack.target + ' for ' + attack.damage + '!');
