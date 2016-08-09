60c0428dda3f3ede4556422253930869cf0c52d4
Marc Johnston <mjohnston@konradgroup.com>
2016-08-09 15:16:22 -0400

    - add interval for dragon attack - update first character HP - output to console

--- app/main.js
+++ app/main.js
@@ -1,2 +1,10 @@
 "use strict";
+var Rx_1 = require('rxjs/Rx');
+var dragonAttack = Rx_1.Observable.interval(3000);
+dragonAttack.subscribe(function (x) {
+    console.log('Dragon attacks for : 10 damage');
+    var el = document.querySelector('.player-list .player:first-child .current-hit-points');
+    var currentHP = parseInt(el.innerHTML);
+    el.innerHTML = currentHP - 10;
+});
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -1,2 +1,10 @@
 import {Observable} from 'rxjs/Rx'
 
+const dragonAttack = Observable.interval(3000);
+
+dragonAttack.subscribe((x) => {
+	console.log('Dragon attacks for : 10 damage');
+	var el = document.querySelector('.player-list .player:first-child .current-hit-points')
+	var currentHP = parseInt(el.innerHTML)
+	el.innerHTML = currentHP - 10;
+});
