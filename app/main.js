8e0c9c779febdefb47fe74b0a827ffc2b14690d6
Marc Johnston <mjohnston@konradgroup.com>
2016-08-09 15:27:42 -0400

    - add observable from event on barbarian attack button - output event

--- app/main.js
+++ app/main.js
@@ -1,10 +1,15 @@
 "use strict";
 var Rx_1 = require('rxjs/Rx');
 var dragonAttack = Rx_1.Observable.interval(3000);
+var barbarianAttackButton = document.querySelector('.player-list .player:first-child .move.attack');
 dragonAttack.subscribe(function (x) {
     console.log('Dragon attacks for : 10 damage');
     var el = document.querySelector('.player-list .player:first-child .current-hit-points');
     var currentHP = parseInt(el.innerHTML);
     el.innerHTML = currentHP - 10;
+});
+var barbarianAttack = Rx_1.Observable.fromEvent(barbarianAttackButton, 'click');
+barbarianAttack.subscribe(function (x) {
+    console.log('got click, here is the event :' + x);
 });
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -2,9 +2,17 @@ import {Observable} from 'rxjs/Rx'
 
 const dragonAttack = Observable.interval(3000);
 
+const barbarianAttackButton = document.querySelector('.player-list .player:first-child .move.attack');
+
 dragonAttack.subscribe((x) => {
 	console.log('Dragon attacks for : 10 damage');
-	var el = document.querySelector('.player-list .player:first-child .current-hit-points')
-	var currentHP = parseInt(el.innerHTML)
+	var el = document.querySelector('.player-list .player:first-child .current-hit-points');
+	var currentHP = parseInt(el.innerHTML);
 	el.innerHTML = currentHP - 10;
 });
+
+const barbarianAttack = Observable.fromEvent(barbarianAttackButton, 'click');
+
+barbarianAttack.subscribe((x) => {
+	console.log('got click, here is the event :' + x);
+})
