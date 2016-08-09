e6b997a7d3be17adea79608ea72aed00f03edc76
Marc Johnston <mjohnston@konradgroup.com>
2016-08-09 16:09:24 -0400

    - make consistent 'attack' object - create allAttacks stream - remove logs from other stream - create specific logger subscription

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,12 @@
 import {Observable} from 'rxjs/Rx'
 
-const dragonAttack = Observable.interval(3000);
+const dragonAttack = Observable.interval(3000)
+						.mapTo({source: 'Dragon', target: 'Barbarian', damage: 10});
 
 const barbarianAttackButton = document.querySelector('.player-list .player:first-child .move.attack');
 
 dragonAttack.subscribe((x) => {
-	console.log('Dragon attacks for : 10 damage');
+	// console.log('Dragon attacks for : 10 damage');
 	var el = document.querySelector('.player-list .player:first-child .current-hit-points');
 	var currentHP = parseInt(el.innerHTML);
 	el.innerHTML = currentHP - 10;
@@ -13,11 +14,17 @@ dragonAttack.subscribe((x) => {
 
 const barbarianAttack = Observable.fromEvent(barbarianAttackButton, 'click')
 							.throttleTime(1000)
-							.mapTo({ name: 'Barbarian', damage: 10 });
+							.mapTo({ source: 'Barbarian', target: 'Dragon', damage: 10 });
+
+const allAttacks = Observable.merge(barbarianAttack, dragonAttack);							
 
 barbarianAttack.subscribe((x) => {
-	console.log(x.name + ' attacks for : ' + x.damage + ' damage');
+	// console.log(x.name + ' attacks for : ' + x.damage + ' damage');
 	var el = document.querySelector('.enemy-list .enemy:first-child .current-hit-points');
 	var currentHP = parseInt(el.innerHTML);
 	el.innerHTML = currentHP - x.damage;
 });
+
+const consoleLogger = allAttacks.subscribe((attack) => {
+	console.log(attack.source + ' hits ' + attack.target + ' for ' + attack.damage + '!')
+})
