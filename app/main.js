01d19feed6a0a543b6208d6723f33fa6e6566c16
Marc Johnston <mjohnston@konradgroup.com>
2016-08-11 16:55:53 -0400

    WIP of shield

--- app/main.js
+++ app/main.js
@@ -2,19 +2,46 @@
 var Rx_1 = require('rxjs/Rx');
 var barbarianAttackButton = document.querySelector('.player-list .player:first-child .move.attack');
 var rogueAttackButton = document.querySelector('.player-list .player:last-child .move.attack');
-var dragonAttack = Rx_1.Observable.interval(3000)
-    .mapTo({ source: 'Dragon', target: 'Barbarian', damage: 10 });
+var healerShieldButton = document.querySelector('.player-list .move.shield');
+var dragonAttack = Rx_1.Observable.interval(500)
+    .map(function () {
+    return { source: 'Dragon', target: 'Barbarian', damage: 10 };
+});
 var barbarianAttack = Rx_1.Observable.fromEvent(barbarianAttackButton, 'click')
     .throttleTime(1000)
     .mapTo({ source: 'Barbarian', target: 'Dragon', damage: 10 });
 var poisonStrikeAttack = Rx_1.Observable.of({ source: 'Rogue', target: 'Dragon', damage: 6 }, { source: 'Rogue', target: 'Dragon', damage: 2 }, { source: 'Rogue', target: 'Dragon', damage: 2 }, { source: 'Rogue', target: 'Dragon', damage: 2 }, { source: 'Rogue', target: 'Dragon', damage: 2 })
-    .zip(Rx_1.Observable.interval(500).take(5), function (left, right) { return left; });
+    .zip(Rx_1.Observable.interval(500).take(5), function (left, right) { console.log('poison'); return left; });
 var rogueAttack = Rx_1.Observable.fromEvent(rogueAttackButton, 'click')
     .throttleTime(1000)
     .switchMapTo(poisonStrikeAttack);
-var allAttacks = Rx_1.Observable.merge(barbarianAttack, dragonAttack, rogueAttack);
+var autoAttacks = Rx_1.Observable.merge(barbarianAttack, dragonAttack, rogueAttack);
+var allAttacks = new Rx_1.Subject();
+var subAllAttacks = autoAttacks.subscribe(allAttacks);
+var thinger = autoAttacks.do(function (x) { return console.log(x); });
 var dragonDamage = allAttacks.filter(function (x) { return x.target === 'Dragon'; });
-var barbarianDamage = allAttacks.filter(function (x) { return x.target === 'Barbarian'; });
+var barbarianDamage = allAttacks.filter(function (x) { return x.target === 'Barbarian'; }).share();
+function getShieldDamageOutput() {
+    return barbarianDamage
+        .take(3)
+        .map(function (attack) {
+        var reduction = attack.damage * 0.3;
+        attack.damage = attack.damage - reduction;
+        attack.reduction = reduction;
+        return attack;
+    })
+        .reduce(function (acc, attack) {
+        acc.damage += attack.reduction;
+        return acc;
+    }, { source: 'Healer', target: 'Dragon', damage: 0 })
+        .do(function (attack) {
+        allAttacks.next(attack);
+    });
+}
+;
+var healerShieldAttack = Rx_1.Observable.fromEvent(healerShieldButton, 'click')
+    .switchMap(getShieldDamageOutput);
+healerShieldAttack.subscribe(console.log);
 dragonDamage.subscribe(function (x) {
     var el = document.querySelector('.enemy-list .enemy:first-child .current-hit-points');
     var currentHP = parseInt(el.innerHTML);
--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,13 @@
-import {Observable} from 'rxjs/Rx'
+import {Observable, Subject} from 'rxjs/Rx'
 
 const barbarianAttackButton = document.querySelector('.player-list .player:first-child .move.attack');
 const rogueAttackButton = document.querySelector('.player-list .player:last-child .move.attack');
+const healerShieldButton = document.querySelector('.player-list .move.shield');
 
-const dragonAttack = Observable.interval(3000)
-						.mapTo({source: 'Dragon', target: 'Barbarian', damage: 10});
+const dragonAttack = Observable.interval(500)
+						.map(function () { 
+							return {source: 'Dragon', target: 'Barbarian', damage: 10}
+						});
 
 const barbarianAttack = Observable.fromEvent(barbarianAttackButton, 'click')
 							.throttleTime(1000)
@@ -19,17 +22,44 @@ const poisonStrikeAttack = Observable.of(
 	)
 	.zip(
 		Observable.interval(500).take(5),
-		function (left, right) { return left; }
+		function (left, right) { console.log('poison'); return left; }
 	);
 
 const rogueAttack = Observable.fromEvent(rogueAttackButton, 'click')
 							.throttleTime(1000)
 							.switchMapTo(poisonStrikeAttack);
 
-const allAttacks = Observable.merge(barbarianAttack, dragonAttack, rogueAttack);							
+const autoAttacks = Observable.merge(barbarianAttack, dragonAttack, rogueAttack);	
+const allAttacks = new Subject();
+const subAllAttacks = autoAttacks.subscribe(allAttacks);
 
+const thinger = autoAttacks.do((x) => console.log(x));
 const dragonDamage = allAttacks.filter(x => { return x.target === 'Dragon'})
-const barbarianDamage = allAttacks.filter(x => { return x.target === 'Barbarian'})
+const barbarianDamage = allAttacks.filter(x => { return x.target === 'Barbarian'}).share()
+
+function getShieldDamageOutput () { return barbarianDamage
+	.take(3)
+	.map(attack => {
+		var reduction = attack.damage * 0.3;
+		attack.damage = attack.damage - reduction;
+		attack.reduction = reduction;
+		return attack;
+	})
+	.reduce((acc, attack) => {
+		acc.damage += attack.reduction;
+		return acc;
+	}, { source: 'Healer', target: 'Dragon', damage: 0 })
+	.do((attack) => {
+		allAttacks.next(attack);
+	});
+};
+
+const healerShieldAttack = Observable.fromEvent(healerShieldButton, 'click')
+						.switchMap(getShieldDamageOutput)
+
+
+healerShieldAttack.subscribe(console.log)
+
 
 dragonDamage.subscribe((x) => {
 	var el = document.querySelector('.enemy-list .enemy:first-child .current-hit-points');
